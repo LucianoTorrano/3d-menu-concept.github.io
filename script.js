8287fe58271f79c3ee14ca06cfa2dae20076f494
Luciano Torrano <lucianotorrano.dev@gmail.com>
2022-10-05 14:41:10 -0300

    sound added

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-const body = document.getElementById('body');
-const menuAir = document.getElementById('air');
-const menuWater = document.getElementById('water');
-const menuEarth = document.getElementById('earth');
-const menuFire = document.getElementById('fire');
-const liquidElement = document.getElementById('liquid');
+const body = document.getElementById("body");
+const menuAir = document.getElementById("air");
+const menuWater = document.getElementById("water");
+const menuEarth = document.getElementById("earth");
+const menuFire = document.getElementById("fire");
+const liquidElement = document.getElementById("liquid");
 
-const resetColor = 'rgb(158,158,158)';
-const airColor = 'rgb(0, 46, 31)';
-const waterColor = 'rgb(0, 16, 41)';
-const earthColor = 'rgb(32, 17, 3)';
-const fireColor = 'rgb(24, 3, 0)';
+const resetColor = "rgb(158,158,158)";
+const airColor = "rgb(0, 46, 31)";
+const waterColor = "rgb(0, 16, 41)";
+const earthColor = "rgb(32, 17, 3)";
+const fireColor = "rgb(24, 3, 0)";
 
+eventHandler(menuAir, airColor, resetColor, body, liquidElement);
+eventHandler(menuWater, waterColor, resetColor, body, liquidElement);
+eventHandler(menuEarth, earthColor, resetColor, body, liquidElement);
+eventHandler(menuFire, fireColor, resetColor, body, liquidElement);
 
-eventHandler(menuAir,airColor,resetColor,body,liquidElement);
-eventHandler(menuWater,waterColor,resetColor,body,liquidElement);
-eventHandler(menuEarth,earthColor,resetColor,body,liquidElement);
-eventHandler(menuFire,fireColor,resetColor,body,liquidElement);
-
-
-function eventHandler(menuItem,elementColor,resetColor,target,liquidElement){
-    
-    menuItem.addEventListener('click',()=>{
-        let evento = liquidElement.dispatchEvent(new KeyboardEvent('keydown', {key: 4 , charCode: 52,code: 'Digit4', path: '[body#liquid, html, document, Window]'}));
-        console.log(evento)
-    })
-    menuItem.addEventListener('mouseover',()=>{
-        changeEnviroment(elementColor,target)
-    })
-    menuItem.addEventListener('mouseout',()=>{
-        resetEnviroment(resetColor,target)
-    })
+function eventHandler(
+  menuItem,
+  elementColor,
+  resetColor,
+  target
+) {
+  menuItem.addEventListener("mouseover", () => {
+    changeEnviroment(elementColor, target);
+  });
+  menuItem.addEventListener("mouseout", () => {
+    resetEnviroment(resetColor, target);
+  });
 }
 
-function changeEnviroment(elementColor, target){
-    target.style.background = elementColor;
+function changeEnviroment(elementColor, target) {
+  target.style.background = elementColor;
 }
 
-function resetEnviroment(resetColor,target){
-    target.style.background = resetColor;
+function resetEnviroment(resetColor, target) {
+  target.style.background = resetColor;
 }
